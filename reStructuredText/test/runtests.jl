8cc71f801b91782d757f30a018c66c6feae992a9
Apua <apua.a.aa@gmail.com>
2019-05-11 13:04:19 +0000

    Renamed package

--- reStructuredText/test/runtests.jl
+++ reStructuredText/test/runtests.jl
@@ -6,6 +6,7 @@ using Test: @inferred
 # test others
 using Test: @test_throws, @test_logs, @test_deprecated, @test_warn, @test_nowarn, @test_broken
 
-@time using PyCall
+using PyCall
 
 include("./test_tables.jl")
+include("./test_paragraphs.jl")
