04051b36dee09cfb25c78a5f5285ac394a99b2ec
Apua <Apua.A.Aa@gmail.com>
2019-06-16 22:40:12 +0800

    Fixed paragraph spec and added literalblock spec

--- reStructuredText/src/quickspec.jl
+++ reStructuredText/src/quickspec.jl
@@ -2,17 +2,17 @@ module inline end
 ####################
 module section end
 module paragraph
-    emptystring = ""  # ·
+    emptystring = ""  # · → Body.eof
     emptyline1 = "\n"  # · → Body.blank → Body.eof
     emptyline2 = "\n\n"  # · → Body.blank → Body.eof
     onestring = "AAA"  # · → Body.text → Text.eof
     oneline1 = "AAA\n"  # · → Body.text → Text.eof
-    oneline2 = "AAA\n\n"  # · → Body.text → Text.eof
+    oneline2 = "AAA\n\n"  # · → Body.text → Text.blank → Body.eof
     twostrings = "AAA\nBBB"  # · → Body.text → Text.text → Body.eof
     twolines = "AAA\nBBB\n"  # · → Body.text → Text.text → Body.eof
     threelines = "AAA\nBBB\nCCC\n"   # · → Body.text → Text.text → Body.eof
     twoblocks = "AAA\n\nBBB\nCCC\n\n"  # · → Body.text → Text.blank → Body.text → Text.text → Body.eof
-end
+    end
 module transition end
 ####################
 module bulletlist end
@@ -22,6 +22,16 @@ module fieldlist end
 module optionlist end
 ####################
 module literalblock
+    emptystring = "::"  # · → Body.line → Line.eof × Body.text → Text.eof
+    emptyline = "::\n"  # · → Body.line → Line.eof × Body.text → Text.eof
+    onestring = "AAA::"  # · → Body.text → Text.eof
+    oneline = "AAA\n::"  # · → Body.text → Text.underline × Text.text → Body.eof
+    twostrings = "AAA\nBBB::"  # · → Body.text → Text.text → Body.eof
+    twoslines = "AAA\nBBB\n::"  # · → Body.text → Text.text → Body.eof
+
+    onelinecontent = "AAA::\n\n  BBB"  # · → Body.text → Text.blank → Body.eof
+    twolinescontent = "AAA\nBBB::\n\n  CCC"  # · → Body.text → Text.text→ Body.eof
+
     """
     case 1::
 
