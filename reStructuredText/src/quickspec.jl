a4a66669b99b3a04e9660ec2b2c77f7a25886d82
Apua <Apua.A.Aa@gmail.com>
2019-06-22 14:33:26 +0800

    Updated specs

--- reStructuredText/src/quickspec.jl
+++ reStructuredText/src/quickspec.jl
@@ -33,23 +33,32 @@ module literalblock
     twolinescontent = "AAA\nBBB::\n\n  CCC"  # · → Body.text → Text.text→ Body.eof
 
     """
-    AAA\nBBB\n
-    AAA\nBBB\n\n
-        ->   context = (AAA, BBB) ; line = ""     => empty line, i.e. State(:Body)
-
-    AAA\n::\n
-    AAA\n::\n\n
-        ->   context = (AAA,) ; line = ""         => literal block
-
-    AAA\nBBB\n CCC\n
-        ->   context = (AAA, BBB) ; line = " CCC" => unexpected indent and quote block
-
-    AAA\nBBB::\n
-        ->   context = (AAA, BBB:) ; line = ""    => literal block
-
-    AAA\nBBB   ::\n
-        ->   context = (AAA, BBB) ; line = ""     => literal block
-
+    warn_with_different_lines = [
+        :emptystring => ( # · → Body.line → Line.eof × Body.text → Text.eof
+            "::",
+            doc(warn_literal_notfound),
+            ),
+        :emptyline => ( # · → Body.line → Line.eof × Body.text → Text.eof
+            "::\n",
+            doc(warn_literal_notfound),
+            ),
+        :onestring => ( # · → Body.text → Text.eof
+            "AAA::",
+            doc(node(:paragraph, "AAA"), warn_literal_notfound),
+            ),
+        :oneline => ( # · → Body.text → Text.underline × Text.text → Body.eof
+            "AAA\n::",
+            doc(info_title_underline, node(:paragraph, "AAA"), warn_literal_notfound),
+            ),
+        :twostrings => ( # · → Body.text → Text.text → Body.eof
+            "AAA\nBBB::",
+            doc(node(:paragraph, "AAA", "BBB"), warn_literal_notfound),
+            ),
+        :twolines => ( # · → Body.text → Text.text → Body.eof
+            "AAA\nBBB\n::",
+            doc(node(:paragraph, "AAA", "BBB"), warn_literal_notfound),
+            ),
+        ]
     """
     end
 module blockquote
@@ -62,3 +71,106 @@ module doctestblock end
 module tables end
 ####################
 module explict end
+
+
+raw"""
+=============================
+Double quote related cases
+=============================
+
+info_incomplete_section
+    msg(info, "Possible incomplete section title.",
+        "Treating the overline as ordinary text because it's so short.")
+
+info_title_underline
+    msg(info, "Possible title underline, too short for the title.",
+        "Treating it as ordinary text because it's so short.")
+
+info_blankline_missing
+    msg(info, "Blank line missing before literal block (after the "::")? Interpreted as a definition list item.")
+
+warn_literal_notfound
+    msg("WARNING", "Literal block expected; none found.")
+
+warn_definitionlist_endswithout_blankline
+    msg(warn, "Definition list ends without a blank line; unexpected unindent.")
+
+warn_blockquote_endswithout_blankline
+    msg(warn, "Block quote ends without a blank line; unexpected unindent.")
+
+--------------------
+
+:: warn_literal_notfound
+::\n warn_literal_notfound
+
+--------------------
+
+::\n:
+    info_incomplete_section
+    info_title_underline
+    paragraph("::", ":")
+::\n::
+    info_incomplete_section
+    section(title("::"))
+
+--------------------
+
+::A
+    paragraph("::A")
+::\nA
+    info_incomplete_section
+    paragraph("::", "A")
+::\nA\n:
+    info_incomplete_section
+    paragraph("::", "A", ":")
+::\nA\n::
+    section(title("A"))
+::\nAAA
+    info_incomplete_section
+    paragraph("::", "AAA")
+::\n AAA
+    info_incomplete_section
+    definition_list(definition_list_item(term("::"), definition(
+        info_blankline_missing,
+        paragraph("AAA"),
+        )))
+::\n ::
+    info_incomplete_section
+    definition_list(definition_list_item(term("::"), definition(
+        info_blankline_missing,
+        warn_literal_notfound,
+        )))
+::\nA\n:
+    info_incomplete_section
+    paragraph("::", "A", ":")
+
+--------------------
+
+::\nA\n:: section(title("A"))
+::\n A\n:: section(title("A"))
+::\n  A\n::
+    info_incomplete_section
+    definition_list(definition_list_item(term("::"), definition(
+        info_blankline_missing,
+        paragraph("A")
+        )))
+    warn_definitionlist_endswithout_blankline
+    warn_literal_notfound
+
+--------------------
+
+::\nA\n:: section(title("A"))
+::\nA:::
+    info_incomplete_section
+    paragraph("::", "AA", "::")
+    warn_literal_notfound
+
+--------------------
+
+A\n:: section(title("A"))
+AA\n:: section(title("AA"))
+ A\n::
+    block_quote(paragraph("A"))
+    warn_blockquote_endswithout_blankline
+    warn_literal_notfound
+"""
